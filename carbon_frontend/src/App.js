99f5919fc4a2759c2806e64fa48b9e69faac9a67
Rutubhimani28 <rutubhimani.specscale@gmail.com>
2024-05-23 17:56:29 +0530

    p

---
+++ carbon_frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LandingPage from '../src/components/landingPage/index'
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
