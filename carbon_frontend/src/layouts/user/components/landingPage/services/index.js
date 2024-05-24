382c4b8932ff36553b308c64d4d41d2173256e49
Rutubhimani28 <rutubhimani.specscale@gmail.com>
2024-05-24 15:49:52 +0530

    "Removed Header and Footer components from App.js, added them to Index.js files in landingPage/home and services, and updated Goal.js with new imports and UI elements."

--- carbon_frontend/src/layouts/user/components/landingPage/services/index.js
+++ carbon_frontend/src/layouts/user/components/landingPage/services/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Banner from './banner'
 import Category from './category';
+import Header from '../../header/header';
+import Footer from '../../footer/footer';
 
 const Index = () => {
     return (
         <div>
+            <Header />
             <Banner />
             <Category />
+            <Footer />
         </div>
     )
 }
