169e52db2580a6510d96e1bf4ef22c45730c407c
Denish-prolink <denish.prolink@gmail.com>
2024-05-28 17:35:55 +0530

    set contact

--- carbon_frontend/src/layouts/user/components/contactUs/ContactUs.js
+++ carbon_frontend/src/layouts/user/components/contactUs/ContactUs.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 import ContactUsForm from './ContactUsForm'
 
 const ContactUs = () => {
   return (
-    <div>
-          Contact Us
-        <ContactUsForm />
+    <div className='main py-5'>
+      <p className="text-center pt-3 fontFamily main fw-bold fs-5">
+        For enquiries, please share your details, and we’ll respond within 24-48 hours. Alternatively, you can email us
+        at  <Link className=" text-decoration-none" style={{ color: '#4edceb' }}>askme@gosustainable.ai. </Link>
+      </p>
+      <ContactUsForm />
     </div>
   )
 }
