6e25adf7cc15fc1e7b069212ac13cffc07441264
Denish-prolink <denish.prolink@gmail.com>
2024-05-28 15:14:05 +0530

    contact us api

--- carbon_backend/routes/serverRoutes.js
+++ carbon_backend/routes/serverRoutes.js
@@ -2,11 +2,13 @@ import express from 'express';
 import userRoutes from './authRoutes.js';
 import customFieldRoutes from './customFieldRoutes.js';
 import digitalContentRoutes from './digitalContentRoutes.js'
+import contactUsRoutes from './contactUsRoutes.js'
 
 const router = express.Router();
 
 router.use('/auth', userRoutes);
 router.use('/custom-field', customFieldRoutes);
 router.use('/digitalContent', digitalContentRoutes);
+router.use('/contactUs', contactUsRoutes);
 
 export default router
