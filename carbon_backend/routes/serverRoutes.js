e3508d9cb0789412df76df34fe688bdd035389e0
Denish-prolink <denish.prolink@gmail.com>
2024-05-28 17:07:33 +0530

    create api for event form

--- carbon_backend/routes/serverRoutes.js
+++ carbon_backend/routes/serverRoutes.js
@@ -3,6 +3,7 @@ import userRoutes from './authRoutes.js';
 import customFieldRoutes from './customFieldRoutes.js';
 import digitalContentRoutes from './digitalContentRoutes.js'
 import contactUsRoutes from './contactUsRoutes.js'
+import eventRoutes from './eventRoutes.js'
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.use('/auth', userRoutes);
 router.use('/custom-field', customFieldRoutes);
 router.use('/digitalContent', digitalContentRoutes);
 router.use('/contactUs', contactUsRoutes);
+router.use('/events', eventRoutes);
 
 export default router
