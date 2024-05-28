9748c408d60c490ea0b1d31f91e99f8a83fc5d64
Rutubhimani28 <rutubhimani.specscale@gmail.com>
2024-05-28 17:58:40 +0530

    Added animation classes to ContactUs paragraph element.

--- carbon_frontend/src/layouts/user/components/contactUs/ContactUs.js
+++ carbon_frontend/src/layouts/user/components/contactUs/ContactUs.js
@@ -4,7 +4,7 @@ import ContactUsForm from './ContactUsForm'
 const ContactUs = () => {
   return (
     <div className='main py-5'>
-      <p className="text-center pt-3 fontFamily main fw-bold fs-5">
+      <p className="text-center pt-3 fontFamily main fw-bold fs-5 wow animate__animated animate__fadeInUp animate__slow">
         For enquiries, please share your details, and we’ll respond within 24-48 hours. Alternatively, you can email us
         at  <Link className=" text-decoration-none" style={{ color: '#4edceb' }}>askme@gosustainable.ai. </Link>
       </p>
