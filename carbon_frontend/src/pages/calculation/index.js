f0875057ccb1c341c2ed51821f52edb1678bfb93
Rutubhimani28 <rutubhimani.specscale@gmail.com>
2024-05-29 18:12:47 +0530

    "Updated tab icons and labels in Calculation page"

--- carbon_frontend/src/pages/calculation/index.js
+++ carbon_frontend/src/pages/calculation/index.js
@@ -37,11 +37,11 @@ const Calculation = () => {
                         aria-label="scrollable auto tabs example"
                     >
                         <Tab icon={<img src={production} alt='' width={35} />} label="Production" />
-                        <Tab icon={<img src={Travel} alt='' width={35} />} label="Air Freight" />
+                        <Tab icon={<img src={Logistics} alt='' width={35} />} label="Logistics" />
                         <Tab icon={<img src={Food} alt='' width={35} />} label="Food" />
-                        <Tab icon={<img src={Energy} alt='' width={35} />} label="Energy Updated" />
+                        <Tab icon={<img src={Energy} alt='' width={35} />} label="Energy" />
                         <Tab icon={<img src={Travel} alt='' width={35} />} label="Travel" />
-                        <Tab icon={<img src={digital} alt='' width={35} />} label="Digital Content" />
+                        <Tab icon={<img src={digital} alt='' width={35} />} label="Digital" />
                         <Tab icon={<img src={Transportation} alt='' width={35} />} label="Local Transportation" />
                         <Tab icon={<img src={Accommodation} alt='' width={35} />} label="Accomodation" />
                         <Tab icon={<img src={Waste} alt='' width={35} />} label="Waste" />
