57cd0211c54b4517be2623c48a0f9c33292adbdf
Denish-prolink <denish.prolink@gmail.com>
2024-06-07 11:59:28 +0530

    "Comment out padding styles in DashboardLayout.js"

--- carbon_frontend/src/layouts/dashboard/DashboardLayout.js
+++ carbon_frontend/src/layouts/dashboard/DashboardLayout.js
@@ -25,11 +25,11 @@ const Main = styled('div')(({ theme }) => ({
   overflow: 'auto',
   minHeight: '100%',
   paddingTop: APP_BAR_MOBILE + 24,
-  paddingBottom: theme.spacing(10),
+  // paddingBottom: theme.spacing(10),
   [theme.breakpoints.up('lg')]: {
-    paddingTop: APP_BAR_DESKTOP + 24,
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(2),
+    // paddingTop: APP_BAR_DESKTOP + 24,
+    // paddingLeft: theme.spacing(2),
+    // paddingRight: theme.spacing(2),
   },
 }));
 
