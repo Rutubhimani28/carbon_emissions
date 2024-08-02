cf4e5caef2c251c9843df9cd90fd88f4472fc2bf
“raj <rajkumar.prolink@gmail.com>
2024-08-02 11:16:06 +0530

    rendering videos based on safari and Mozila

--- carbon_frontend/src/layouts/user/components/esgAdvisory/esgAdvisory.js
+++ carbon_frontend/src/layouts/user/components/esgAdvisory/esgAdvisory.js
@@ -8,11 +8,13 @@ import Organisations from '../../assets/images/Organisations2.jpg'
 import Hospitality from '../../assets/images/Hospitality.jpeg'
 import Exhibitions from '../../assets/images/Exhibition2.jpeg'
 import ESGAdvisoryServices from '../../assets/images/ESG Advisory Services.jpeg';
-import BannerVid from '../../assets/images/NetZero Consulting.mp4';
+import bannerVideo1 from '../../assets/images/NetZero Consulting.mp4';
+import bannerVideo2 from '../../assets/images/NetZero Consulting.mov';
 
 const EsgAdvisory = () => {
 
     const videoRef = useRef();
+    const appCodeName = navigator?.appCodeName;
 
     useEffect(() => {
         videoRef.current.playbackRate = 0.5;
@@ -63,48 +65,35 @@ const EsgAdvisory = () => {
         //     </div>
         // </div>
 
-
         <div>
-            {/* <video
-                src="https://gosustainable.ai/wp-content/uploads/2024/01/Sustainable-Events-Video.mp4" width={"100%"} autoPlay muted>
-                <track kind="captions" src="captions.vtt" label="English" />
-                Sorry, your browser doesn't support embedded videos, but don't worry, you can
-                <a href="https://gosustainable.ai/wp-content/uploads/2024/01/Sustainable-Events-Video.mp4">download it</a>
-                and watch it with your favorite video player!
-            </video> */}
-
             {/* <div>
                 <img src={ESGAdvisoryServices} alt="ESG Advisory Services" width="100%" style={{ objectFit: "cover", maxHeight: "650px" }} />
             </div> */}
 
             <div style={{ maxWidth: "100%", maxHeight: "750px", overflow: 'hidden' }}>
                 <video
-                    src={BannerVid}
                     width={"100%"}
                     autoPlay
                     muted
                     preload
                     ref={videoRef}
                     loop
+                    playsInline
                     style={{ objectFit: 'fill', height: '75vh' }}
                 >
+                    <source src={appCodeName === "Safari" ? bannerVideo2 : bannerVideo1} type={appCodeName === "Safari" ? "video/mov" : "video/mp4"} />
                     <track kind="captions" src="captions.vtt" label="English" />
                     Sorry, your browser doesn't support embedded videos, but don't worry, you can
                     <a href="https://gosustainable.ai/wp-content/uploads/2024/01/Sustainable-Events-Video.mp4">download it</a>
                     and watch it with your favorite video player!
                 </video>
             </div>
-
             <div className='container'>
                 <div style={{ paddingTop: '48px', paddingBottom: '42px' }}>
-                    {/* <Typography variant='h6' className='text-center fs-3 pt-4 fontFamily fw-bold wow animate__animated animate__fadeInUp animate__slow' >NetZero Consulting</Typography> */}
                     <p className='pb-4 fontFamily wow animate__animated animate__fadeInUp animate__slow fs-5' style={{ textAlign: 'justify' }}>We provide NetZero Consulting to develop sustainable business strategies for industries. Our objective is to foster collaboration and support within these industries to collectively meet climate goals. By adopting sustainable measures, industries can reduce their operational costs while creating added value and acting responsibly. With NetZero Consulting you can improve efficiency, control costs, and achieve your sustainability goals.</p>
-                    {/* <p className='pb-3 fontFamily wow animate__animated animate__fadeInUp animate__slow fs-5' style={{ textAlign: 'justify' }}>Our <Link to="/netzero-platform" style={{ color: "#ffffd9", textDecoration: 'none' }}>NetZero Platform</Link> supports data-driven decisions, enhances brand reputation, and aligns with your ESG objectives, reflecting our shared commitment to sustainability and compassion for future generations.</p> */}
                 </div>
                 <div>
-                    {/* <Typography variant='h6' className=' fs-3 pt-4 fontFamily fw-bold wow animate__animated animate__fadeInUp animate__slow' >Services</Typography>
-                    <p className='  py-3 pb-3 fontFamily wow animate__animated animate__fadeInUp animate__slow'>Our ESG advisory services assist industries in reducing their carbon footprint, creating a long-term business value.</p> */}
-                    <img src={Services} alt="services" className='pb-5 pb-5 wow animate__animated animate__fadeInUp animate__slow' />
+                   <img src={Services} alt="services" className='pb-5 pb-5 wow animate__animated animate__fadeInUp animate__slow' />
                 </div>
                 <div>
                     <Typography variant='h6' className=' fs-1 pt-4 fontFamily fw-bold wow animate__animated animate__fadeInUp animate__slow text-center' >Industries</Typography>
@@ -128,27 +117,6 @@ const EsgAdvisory = () => {
 
                     </Grid>
                 </div>
-                {/* <div className='pt-2 wow animate__animated animate__fadeInUp animate__slow' id="organisation">
-                    <Typography variant='h6' className='fontFamily fw-bold fs-3'>Organisations</Typography>
-                    <p className='py-3' style={{ textAlign: 'justify' }}>Our ESG consulting for the Chief Marketing Officer (CMO) vertical transcends routine ESG measures. Understanding the challenges you face, we believe in empowering you to take proactive steps towards sustainability across all your activities.</p>
-                    <p className='' style={{ textAlign: 'justify' }}>Our <Link to="/netzero-platform" style={{ color: "#ffffd9", textAlign: 'justify', textDecoration: 'none' }}>NetZero tool</Link>, tailored specifically for the marketing vertical, diligently measures the carbon footprint generated from each activity, aiding in your journey towards sustainability goals and enhancing corporate responsibility. It offers precise data on the carbon footprint from marketing activities, enabling targeted reduction strategies.</p>
-                </div>
-                <div className='my-3 pt-4 wow animate__animated animate__fadeInUp animate__slow' id="agencies">
-                    <Typography variant='h6' className='fontFamily fw-bold fs-3'>Agencies</Typography>
-                    <p className='py-3' style={{ textAlign: 'justify' }}>Agencies supporting organizations (clients) bring a distinctive value proposition by executing sustainable activities throughout the year. Embracing sustainable practices not only sets you apart from the competition but also demonstrates your understanding and support for environmental concerns, attracting more clients and expanding your business opportunities.</p>
-                    <p className='' style={{ textAlign: 'justify' }}>Our <Link to="/netzero-platform" style={{ color: "#ffffd9", textAlign: 'justify', textDecoration: 'none' }}>NetZero Platform</Link> assists in measuring the carbon footprint of the activities you support, positioning you as a sustainable event organizer. It provides accurate data on environmental impact, enhancing your brand reputation and aligning with your companies, and countries climate actions.</p>
-                </div>
-                <div className='my-3 pt-4 wow animate__animated animate__fadeInUp animate__slow' id="hospitality">
-                    <Typography variant='h6' className='fontFamily fw-bold fs-3'>Hospitality</Typography>
-                    <p className='py-3' style={{ textAlign: 'justify' }}>Attracting major companies to host events and exhibitions in your hotel demonstrates your commitment to clients and sustainability. We understand that many organizations seek sustainable hotels, and we're here to help you meet those expectations.</p>
-                    <p className='' style={{ textAlign: 'justify' }}>To fully adhere to sustainable measures, you need a net-zero framework and an <Link to="/netzero-platform" style={{ color: "#ffffd9", textAlign: 'justify', textDecoration: 'none' }}>NetZero Platform</Link> that meticulously tracks the emissions generated during clients' events on your premises. Our emissions tool aids in managing your carbon footprint, attracting climate-conscious clients, and aligning with your overall ESG objectives.</p>
-                </div>
-                <div className='pb-5 pt-4 wow animate__animated animate__fadeInUp animate__slow' id="exhibitions">
-                    <Typography variant='h6' className='fontFamily fw-bold fs-3'>Exhibitions</Typography>
-                    <p className='py-3' style={{ textAlign: 'justify' }}>As an exhibition or congress organizer, you play a pivotal role in bringing together diverse companies, segments, and government bodies, enabling them to showcase their innovations. To fully embrace sustainable practices, we understand your willingness to support climate initiatives.</p>
-                    <p className='' style={{ textAlign: 'justify' }}>A robust governance model, coupled with our ESG advisory services and <Link to="/netzero-platform" style={{ color: "#ffffd9", textAlign: 'justify', textDecoration: 'none' }}>NetZero Platform</Link>, enhances your reputation, attracting eco-conscious exhibitors and attendees. Together, we align with your overall ESG objectives, fostering accountability and environmental stewardship.</p>
-                </div> */}
-                {/* <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} className='py-5  d-flex justify-content-center align-items-center' id="organisation"> */}
                 <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} className='py-5' id="organisation">
                     <Grid item xs={12} md={4} sx={{ order: { md: '1' } }} className='wow animate__animated animate__fadeInLeft animate__slow'>
                         <Box className="organise p-3 " style={{ borderRadius: "20px" }}>
@@ -197,7 +165,6 @@ const EsgAdvisory = () => {
                         <p className='fs-5' style={{ textAlign: 'justify' }}>A robust governance model, coupled with our NetZero Consulting and a <Link to="/netzero-platform" style={{ color: "#ffffd9", textAlign: 'justify', textDecoration: 'none' }}>NetZero Platform</Link>, enhances your reputation, attracting eco-conscious exhibitors and attendees. Together, we align with your overall ESG objectives, fostering accountability and environmental stewardship.</p>
                     </Grid>
                 </Grid>
-
                 <p className='fs-5'>We would like to partner with you to reduce your carbon footprint, fostering long-term business value creation with a deep understanding of your needs.</p>
                 <p className='fontFamily wow animate__animated animate__fadeInUp animate__slow pb-5 mt-1' style={{ textAlign: 'center' }}>
                     <PopupButton
@@ -209,7 +176,6 @@ const EsgAdvisory = () => {
                     />
                     <span className='fs-5'> with us to know more.</span>
                 </p>
-
             </div>
         </div>
     )
--- carbon_frontend/src/layouts/user/components/services/banner.js
+++ carbon_frontend/src/layouts/user/components/services/banner.js
@@ -1,11 +1,13 @@
 import { useEffect, useRef } from 'react';
-import { Box, Grid, Typography } from '@mui/material';
-// import BannerVid2 from '../../assets/images/NetZero Platform- Updated Video.mp4';
-import BannerVid2 from '../../assets/images/NetZero Platform 3.mp4';
+import { Box, Grid } from '@mui/material';
+// import bannerVideo2 from '../../assets/images/NetZero Platform- Updated Video.mp4';
+import bannerVideo1 from '../../assets/images/NetZero Platform 3.mp4';
+import bannerVideo2 from '../../assets/images/NetZero Platform 3.mov';
 
 const Banner = () => {
 
     const videoRef = useRef();
+    const appCodeName = navigator?.appCodeName;
 
     useEffect(() => {
         videoRef.current.playbackRate = 0.5;
@@ -15,15 +17,16 @@ const Banner = () => {
         <div>
             <div style={{ maxWidth: "100%", maxHeight: "750px", overflow: "hidden" }}>
                 <video
-                    src={BannerVid2}
                     width={"100%"}
                     autoPlay
                     muted
                     preload
                     ref={videoRef}
                     loop
+                    playsInline
                     style={{ objectFit: 'fill', height: '75vh' }}
                 >
+                    <source src={appCodeName === "Safari" ? bannerVideo2 : bannerVideo1} type={appCodeName === "Safari" ? "video/mov" : "video/mp4"} />
                     <track kind="captions" src="captions.vtt" label="English" />
                     Sorry, your browser doesn't support embedded videos, but don't worry, you can
                     <a href="https://gosustainable.ai/wp-content/uploads/2024/01/Sustainable-Events-Video.mp4">download it</a>
@@ -36,7 +39,7 @@ const Banner = () => {
 
                     <Grid className='fs-5 py-4 wow animate__animated animate__fadeInRight animate__slow' item sm={12} md={12} style={{ textAlign: 'justify' }}>An AI enabled carbon accounting platform to collect data and assess the complete life cycle of all your marketing and communication activities' carbon footprint. Our platform covers not only digital campaigns and digital marketing but also events, including face-to-face events, virtual events, and public relations events. It encompasses all phases—pre-event, event, and post-event—and marks a significant milestone based on our NetZero framework.</Grid>
 
-                   <Grid className='fs-5 py-4 wow animate__animated animate__fadeInUp animate__slow' item sm={12} style={{ textAlign: 'justify' }}>We earnestly delve into the meticulous examination of the nine diverse categories as outlined below:</Grid>
+                    <Grid className='fs-5 py-4 wow animate__animated animate__fadeInUp animate__slow' item sm={12} style={{ textAlign: 'justify' }}>We earnestly delve into the meticulous examination of the nine diverse categories as outlined below:</Grid>
                 </Grid>
             </Box>
         </div>
