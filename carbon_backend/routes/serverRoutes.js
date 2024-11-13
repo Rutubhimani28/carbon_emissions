97eee1cf2ede6d114b48c6463319f24442d70325
Denish-prolink <denish.prolink@gmail.com>
2024-11-13 14:46:57 +0530

    contact-us route enabled from backend

--- carbon_backend/routes/serverRoutes.js
+++ carbon_backend/routes/serverRoutes.js
@@ -3,7 +3,7 @@ import authRoutes from './authRoutes.js';
 // import customFieldRoutes from './customFieldRoutes.js';
 // import digitalContentRoutes from './digitalContentRoutes.js'
 import emailRoutes from './emailRoutes.js'
-// import contactUsRoutes from './contactUsRoutes.js'
+import contactUsRoutes from './contactUsRoutes.js'
 // import eventRoutes from './eventRoutes.js'
 // import airFreightRoutes from './airFreightRoutes.js'
 import botRoutes from './botRoutes.js'
@@ -18,7 +18,7 @@ router.use('/auth', authRoutes);
 // router.use('/custom-field', customFieldRoutes);
 // router.use('/digitalContent', digitalContentRoutes);
 router.use('/email', emailRoutes);
-// router.use('/contactUs', contactUsRoutes);
+router.use('/contactUs', contactUsRoutes);
 // router.use('/events', eventRoutes);
 // router.use('/airFreight', airFreightRoutes);
 router.use('/bot', botRoutes);
